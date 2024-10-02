ab9525a487185a245094b360278d846aad8c6354
lucasito028 <lucastoshihiroito28@gmail.com>
2024-10-02 17:44:55 -0300

    Finalizando

--- contact/admin.py
+++ contact/admin.py
@@ -10,11 +10,11 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', 'email' , 'create_date', 'description'
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email' , 'create_date', 'description', 'show',
     ordering = '-id',
     list_filter = 'create_date',
     search_fields = 'id', 'first_name', 'last_name', 'phone', 'email' , 'create_date', 'description',
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'first_name', 'last_name',
+    list_editable = 'first_name', 'last_name', 'show',
     list_display_links = 'id', 'phone',
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render #type: ignore
 from contact.models import Contact
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.all()
+    #contacts = Contact.objects.all().order_by('-id')
+    contacts = Contact.objects\
+    .filter(show=True)\
+    .order_by('-id')[:10]
+    print(contacts.query)
     
     context = {
         'contacts': contacts
@@ -12,5 +18,21 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+def contact(request, contact_id):
+    #single_contact  = Contact.objects.get(pk=contact_id).first()
+    
+    single_contact = get_object_or_404(
+        Contact.objects, pk=contact_id, show=True
+    )
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
