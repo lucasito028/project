46bce340d96f82113000c6c10f3142b8855b4b1c
lucasito028 <lucastoshihiroito28@gmail.com>
2024-11-06 16:58:39 -0300

    Melhorias

--- contact/forms.py
+++ contact/forms.py
@@ -3,10 +3,21 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+        )
+    )
+
     class Meta:
         model = models.Contact
         fields = (
             'first_name','last_name','phone',
+            'email','description','category',
+            'picture',
+
         )
         # Método clean é usada quando precisamos validar campos no formulários,
         # este método é chamado antes de salvar os dados na base de dados
